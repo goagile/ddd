910252b109a197dec939031bfc01d962ae32dccc
khardi <kharchilindanila@gmail.com>
2017-07-02 21:18:25 +0300

    order example tests pass

---
+++ mocks_arent_stubs/order/model.py
@@ -0,0 +1,37 @@
+
+
+class Order:
+
+    def __init__(self, product_type, need_quantity):
+        self.product_type = product_type
+        self.need_quantity = need_quantity
+        self.quantity = 0
+
+    def fill(self, stock):
+        is_can_fill = stock.has_inventory(self.product_type)
+        if is_can_fill:
+            self.quantity = stock.unload(self.product_type, self.need_quantity)
+
+    def is_filled(self):
+        return bool(self.quantity != 0)
+
+
+class Warehouse:
+
+    def __init__(self):
+        self.inventory = {}
+
+    def get_inventory(self, product_type):
+        return self.inventory.get(product_type)
+
+    def add(self, product_type, product):
+        self.inventory[product_type] = product
+
+    def unload(self, product_type, quantity):
+        if self.inventory[product_type] < quantity:
+            return 0
+        self.inventory[product_type] -= quantity
+        return quantity
+
+    def has_inventory(self, product_type):
+        return bool(self.inventory.get(product_type, None))
---
+++ mocks_arent_stubs/order/test_orders.py
@@ -0,0 +1,57 @@
+"""
+    link: https://martinfowler.com/articles/mocksArentStubs.html
+"""
+import unittest
+from unittest.mock import MagicMock
+
+from nilsson.mocks_arent_stubs.order.model import Order, Warehouse
+
+TALISKER = 'Talisker'
+HIGHLAND_PARK = 'Highland Park'
+warehouse = Warehouse()
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        warehouse.add(TALISKER, 50)
+        warehouse.add(HIGHLAND_PARK, 25)
+
+    def test_order_is_filled_if_enough_in_warehouse(self):
+        order = Order(TALISKER, 50)
+        order.fill(warehouse)
+
+        self.assertTrue(order.is_filled())
+        self.assertEqual(0, warehouse.get_inventory(TALISKER))
+
+    def test_order_does_not_remove_if_not_enough(self):
+        order = Order(TALISKER, 51)
+        order.fill(warehouse)
+
+        self.assertFalse(order.is_filled())
+        self.assertEqual(50, warehouse.get_inventory(TALISKER))
+
+
+class TestWithMocks(unittest.TestCase):
+
+    def test_filling_removes_inventory_if_in_stock(self):
+        order = Order(TALISKER, 50)
+        warehouse_mock = MagicMock(
+            has_inventory=MagicMock(return_value=True),
+            unload=MagicMock(return_value=50)
+        )
+
+        order.fill(warehouse_mock)
+
+        self.assertTrue(order.is_filled())
+        self.assertEqual(50, order.quantity)
+
+    def test_filling_does_not_remove_if_not_enough_in_stock(self):
+        order = Order(TALISKER, 51)
+        warehouse_mock = MagicMock(
+            has_inventory=MagicMock(return_value=False)
+        )
+
+        order.fill(warehouse_mock)
+
+        self.assertFalse(order.is_filled())
