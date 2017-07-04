bc9cfb402baee87d2ade7d9de5f1f64f76131c97
khardi <kharchilindanila@gmail.com>
2017-07-04 08:55:42 +0300

    products check len

--- start_examples/specification/test.py
+++ start_examples/specification/test.py
@@ -8,20 +8,22 @@ repository.add_product(Color.RED)
 repository.add_product(Color.GREEN, Size.L)
 
 
-class Test(unittest.TestCase):
+class TestRepository(unittest.TestCase):
 
     def test_find_by_color(self):
         expected = 1
-        product = repository.find_by_color(Color.RED)
+        products = repository.find_by_color(Color.RED)
 
-        result = product[0].key
+        result = products[0].key
 
+        self.assertEqual(1, len(products))
         self.assertEqual(expected, result)
 
     def test_find_by_size(self):
         expected = 2
-        product = repository.find_by_size(Size.L)
+        products = repository.find_by_size(Size.L)
 
-        result = product[0].key
+        result = products[0].key
 
+        self.assertEqual(1, len(products))
         self.assertEqual(expected, result)
