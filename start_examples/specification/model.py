8add54fd0e82fcee13efeda83ceddeea0197559f
khardi <kharchilindanila@gmail.com>
2017-07-04 08:54:08 +0300

    test_find_by_size

--- start_examples/specification/model.py
+++ start_examples/specification/model.py
@@ -1,13 +1,22 @@
+
+
 class Color:
     RED = 'Red'
     GREEN = 'Green'
 
 
+class Size:
+    S = 'S'
+    M = 'M'
+    L = 'L'
+
+
 class Product:
 
-    def __init__(self, key, color):
+    def __init__(self, key, color, size):
         self.key = key
         self.color = color
+        self.size = size
 
 
 class Repository:
@@ -15,10 +24,10 @@ class Repository:
     key = 0
     products = []
 
-    def add_product(self, color):
+    def add_product(self, color, size=Size.M):
         new_key = self.key + 1
         self.key = new_key
-        new_product = Product(new_key, color)
+        new_product = Product(new_key, color, size)
         self.products.append(new_product)
 
     def find_by_color(self, color):
--- start_examples/specification/test.py
+++ start_examples/specification/test.py
@@ -1,16 +1,26 @@
 import unittest
 
-from nilsson.start_examples.specification.model import Color, Product, Repository
+from nilsson.start_examples.specification.model import Color, Product, Repository, Size
+
 
 repository = Repository()
-repository.add_product(color=Color.RED)
+repository.add_product(Color.RED)
+repository.add_product(Color.GREEN, Size.L)
 
 
 class Test(unittest.TestCase):
 
-    def test(self):
+    def test_find_by_color(self):
         expected = 1
         product = repository.find_by_color(Color.RED)
+
+        result = product[0].key
+
+        self.assertEqual(expected, result)
+
+    def test_find_by_size(self):
+        expected = 2
+        product = repository.find_by_size(Size.L)
 
         result = product[0].key
 
