501ae020617ba80265f13958acf463068d7d3603
khardi <kharchilindanila@gmail.com>
2017-07-12 06:37:01 +0300

    360 rules

--- start_examples/sector2/model.py
+++ start_examples/sector2/model.py
@@ -7,11 +7,16 @@ class CcwSector:
 
     def contains(self, angle):
         angle = self.rotate_360(angle)
-        return bool(angle <= self.b)
+        result = bool(self.a <= angle <= self.b)
+        return result
 
     def rotate_360(self, angle):
         while angle >= 360:
             angle -= 360
         while angle < 0:
             angle += 360
+        if self.b < self.a:
+            self.b += 360
+            if angle == 0:
+                angle = 360
         return angle
--- start_examples/sector2/test.py
+++ start_examples/sector2/test.py
@@ -4,6 +4,7 @@ from nilsson.start_examples.sector2.model import CcwSector
 
 
 I = CcwSector(0, 90)
+II_III_IV = CcwSector(90, 0)
 
 
 class TestCcwSector(unittest.TestCase):
@@ -30,14 +31,14 @@ class TestCcwSector(unittest.TestCase):
         self.assertTrue(sector.contains(-360))
         self.assertFalse(sector.contains(-360 - 1))
 
-    # def test_II_III_IV_sector_positive_angle(self):
-    #     sector = II_III_IV
-    #     self.assertTrue(I.contains(0))
-    #     self.assertTrue(I.contains(90))
-    #     self.assertFalse(I.contains(90 + 1))
-    #     self.assertFalse(I.contains(180))
-    #     self.assertFalse(I.contains(180 + 90))
-    #     self.assertFalse(I.contains(180 + 90 + 1))
-    #     self.assertTrue(I.contains(360))
-    #     self.assertTrue(I.contains(360 + 90))
-    #     self.assertFalse(I.contains(360 + 90 + 1))
+    def test_II_III_IV_sector_positive_angle(self):
+        sector = II_III_IV
+        self.assertTrue(sector.contains(0))
+        self.assertTrue(sector.contains(90))
+        self.assertFalse(sector.contains(90 - 1))
+        self.assertFalse(sector.contains(0 + 1))
+        self.assertTrue(sector.contains(180))
+        self.assertTrue(sector.contains(180 + 90))
+        self.assertTrue(sector.contains(360))
+    #     self.assertTrue(sector.contains(360 + 90))
+    #     self.assertFalse(sector.contains(360 + 90 + 1))
