25c4d5878c21dcaa1ca7f7541bec2bd3efe530e5
khardi <kharchilindanila@gmail.com>
2017-07-12 21:25:10 +0300

    simple  insert_tr

--- start_examples/js_locale/test_parse.py
+++ start_examples/js_locale/test_parse.py
@@ -4,31 +4,47 @@ import unittest
 
 QUOTES = r'(?:\'|\")'
 
-ru = re.compile(QUOTES + r'([А-Яа-я]+)' + QUOTES)
+ru = re.compile('(' + QUOTES + r'[А-Яа-я]+' + QUOTES + ')')
 
 
 class Test(unittest.TestCase):
 
     def test_findall(self):
-        expected = ['Имя', 'Фамилия']
-        text = "var columns = ['Имя', 'Фамилия'];"
+        expected = ['"Имя"', '"Фамилия"']
+        text = 'var columns = ["Имя", "Фамилия"];'
 
         result = findall(text)
 
         self.assertEqual(expected, result)
 
     def test_split(self):
-        expected = ['var x = ', 'Пуск', '; ']
-        text = "var x = 'Пуск'; "
+        expected = ['var x = ', '"Пуск"', '; ']
+        text = 'var x = "Пуск"; '
 
         result = split(text)
+
+        self.assertEqual(expected, result)
+
+    def test_insert_tr(self):
+        expected = 'var x = TR("Пуск"); '
+        text = 'var x = "Пуск"; '
+
+        result = insert_tr(text)
 
         print(result)
 
         self.assertEqual(expected, result)
 
-    # def test_insert_tr(self):
 
+def insert_tr(line):
+    result = []
+    parts = ru.split(line)
+    for part in parts:
+        p = part
+        if ru.match(part):
+            p = 'TR(' + part + ')'
+        result.append(p)
+    return ''.join(result)
 
 
 def findall(text):
