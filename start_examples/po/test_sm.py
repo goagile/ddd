20e495171a88bb1bc05323bb9ee6c7046b0a685c
khardi <kharchilindanila@gmail.com>
2017-07-18 00:31:06 +0300

    add Controller

--- start_examples/po/test_sm.py
+++ start_examples/po/test_sm.py
@@ -33,16 +33,54 @@ class Test(unittest.TestCase):
 
         self.assertEqual(1, result)
 
+    def test_(self):
+        start = State('start')
+        machine = StateMachine(start)
+        msgid_finded = Event('msgid_finded')
+        waiting_msgstr = State('waiting_msgstr')
+        start.add_transition(waiting_msgstr, event=msgid_finded)
+        controller = Controller(machine, [])
+
+        controller.handle('msgid_finded')
+
+        self.assertEqual(waiting_msgstr, controller.current_state)
+
+
+class Controller:
+
+    def __init__(self, machine, commands):
+        self.machine = machine
+        self.commands = commands
+        self.current_state = machine.start
+
+    def handle(self, event_name):
+        if self.current_state.has_transition(event_name):
+            self.transition_to(self.current_state.target_state(event_name))
+
+    def transition_to(self, target):
+        self.current_state = target
+
+
+class StateMachine:
+
+    def __init__(self, start):
+        self.start = start
+
 
 class State:
 
     def __init__(self, name):
         self.name = name
-        self.transitions = []
+        self.transitions = {}
 
     def add_transition(self, target, event):
-        t = Transition(self, event, target)
-        self.transitions.append(t)
+        self.transitions[event.name] = Transition(self, event, target)
+
+    def has_transition(self, event_name):
+        return bool(event_name in self.transitions)
+
+    def target_state(self, event_name):
+        return self.transitions.get(event_name).target
 
 
 class Transition:
