22a8d229cca366151a2a27f40eddec60ec261f9e
khardi <kharchilindanila@gmail.com>
2017-07-17 21:43:43 +0300

    TestMsgPlural

--- start_examples/po/model.py
+++ start_examples/po/model.py
@@ -17,3 +17,22 @@ class Msg:
     @property
     def paths(self):
         return iter(self.__paths)
+
+
+class MsgPlural(Msg):
+
+    def __init__(self, id, id_plural=''):
+        super().__init__(id)
+        self.__strs = []
+        self.__id_plural = id_plural
+
+    @property
+    def id_plural(self):
+        return self.__id_plural
+
+    @property
+    def strs(self):
+        return self.__strs
+
+    def add_str(self, s):
+        self.__strs.append(s)
---
+++ start_examples/po/test_plural.py
@@ -0,0 +1,37 @@
+import unittest
+
+from nilsson.start_examples.po.model import MsgPlural
+
+
+boxes = MsgPlural(id='Box', id_plural='Boxes')
+
+
+class TestMsgPlural(unittest.TestCase):
+
+    def test_create_msg_with_id(self):
+        self.assertEqual('Box', boxes.id)
+        self.assertEqual('Boxes', boxes.id_plural)
+
+    def test_add_str(self):
+        expected = [
+            'Ящик',
+            'Ящики'
+        ]
+        boxes.add_str('Ящик')
+        boxes.add_str('Ящики')
+
+        result = [s for s in boxes.strs]
+
+        self.assertEqual(expected, result)
+
+    def test_add_path(self):
+        expected = [
+            '../modules/user/x.js:112',
+            '../modules/user/x.js:300'
+        ]
+        boxes.add_path('../modules/user/x.js:112')
+        boxes.add_path('../modules/user/x.js:300')
+
+        result = [p for p in boxes.paths]
+
+        self.assertEqual(expected, result)
