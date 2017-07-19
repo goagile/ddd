b76e1d7a7bb311aaff6a9e8211c90e1a0d94f7c5
khardi <kharchilindanila@gmail.com>
2017-07-19 12:45:56 +0300

    del ExceWriter and ExcelParser

--- start_examples/po/parser/excel_parser.py
+++ start_examples/po/parser/excel_parser.py
@@ -3,38 +3,6 @@ from openpyxl import load_workbook
 from start_examples.po.msg_model.msg_collection import MsgCollection
 
 
-class ExcelParser:
-
-    @classmethod
-    def parse(cls, excel_path):
-        wb = load_workbook(excel_path)
-        ws = wb.get_active_sheet()
-
-        result = MsgCollection()
-
-        for row in ws.rows:
-            id_, msgstrs, paths = row[0].value, row[1].value, row[2].value
-            if not row[0].value:
-                continue
-            if cls.is_plural(id_) or cls.is_plural(msgstrs):
-                ids = cls.split_by_new_line(id_)
-                result.add_msg_plural(
-                    id=ids[0], id_plural=ids[1], strs=cls.split_by_new_line(msgstrs), paths=cls.split_by_new_line(paths)
-                )
-            else:
-                result.add_msg(id=id_, str=msgstrs, paths=cls.split_by_new_line(paths))
-
-        return result
-
-    @classmethod
-    def split_by_new_line(cls, str):
-        return str.split('\n')
-
-    @classmethod
-    def is_plural(cls, value):
-        return bool(len(value.split('\n')) > 1)
-
-
 class RuEnExcelParser:
 
     @classmethod
--- start_examples/po/writer/excel_writer.py
+++ start_examples/po/writer/excel_writer.py
@@ -10,49 +10,6 @@ CLOSE_EXCEL = '\n\t\t!!! Закройте Excel !!!\n'
 COLLECTIONS_MUST_BE_SAME_LENS = 'Коллекции должны быть одинаковой длины'
 
 
-class ExcelWriter:
-
-    @classmethod
-    def write(cls, excel_path, msg_collection):
-        wb = Workbook()
-        ws = wb.get_active_sheet()
-        alignment = Alignment(wrapText=True, vertical='top', shrink_to_fit=True)
-
-        for row_index, msg in enumerate(msg_collection, 2):
-            if msg.is_plural:
-                cell_id = ws.cell(row=row_index, column=1)
-                cell_id.alignment = alignment
-                cell_id.value = join_by_new_line([msg.id, msg.id_plural])
-
-                cell_en = ws.cell(row=row_index, column=2)
-                cell_en.alignment = alignment
-                cell_en.value = join_by_new_line(msg.strs)
-
-                cell_paths = ws.cell(row=row_index, column=3)
-                cell_paths.alignment = alignment
-                cell_paths.value = join_by_new_line(msg.paths)
-            else:
-                cell_id = ws.cell(row=row_index, column=1)
-                cell_id.alignment = alignment
-                cell_id.value = msg.id
-
-                cell_en = ws.cell(row=row_index, column=2)
-                cell_en.alignment = alignment
-                cell_en.value = msg.str
-
-                cell_paths = ws.cell(row=row_index, column=3)
-                cell_paths.alignment = alignment
-                cell_paths.value = join_by_new_line(msg.paths)
-
-            # ws.row_dimensions[row_index].height = 15
-
-        widths = (25, 30, 80, 20, 40)
-        for col, width in enumerate(widths, start=1):
-            ws.column_dimensions[get_column_letter(col)].width = width
-
-        wb.save(excel_path)
-
-
 class RuEnExcelWriter:
 
     def __init__(self):
