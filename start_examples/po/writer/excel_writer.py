b4dc56db61e262a6cb5308b223d212a6f430f570
khardi <kharchilindanila@gmail.com>
2017-07-19 11:39:40 +0300

    names, widths

--- start_examples/po/writer/excel_writer.py
+++ start_examples/po/writer/excel_writer.py
@@ -55,6 +55,8 @@ class ExcelWriter:
 class RuEnExcelWriter:
 
     def __init__(self):
+        self.names = ['Ключ', 'RU', 'EN', 'Новый ключ', 'Новый RU', 'Новый EN', 'Путь']
+        self.widths = [40, 40, 40, 40, 40, 40, 120]
         self.align = Alignment(wrapText=True, vertical='top', shrink_to_fit=True)
         self.wb = Workbook()
         self.ws = self.wb.get_active_sheet()
@@ -93,9 +95,8 @@ class RuEnExcelWriter:
                 ])
 
     def __write_header(self):
-        values = ['Ключ', 'RU', 'EN', 'Новый ключ', 'Новый RU', 'Новый EN', 'Путь']
-        for i, value in enumerate(values, start=1):
-            self.ws.cell(row=1, column=i).value = value
+        for i, name in enumerate(self.names, start=1):
+            self.ws.cell(row=1, column=i).value = name
 
     def __write_values_to(self, row, values):
         for col, value in enumerate(values, start=1):
@@ -108,6 +109,5 @@ class RuEnExcelWriter:
 
     def __set_dimensions(self):
         # ws.row_dimensions[row_index].height = 15
-        widths = (25, 30, 80, 20, 40)
-        for col, width in enumerate(widths, start=1):
+        for col, width in enumerate(self.widths, start=1):
             self.ws.column_dimensions[get_column_letter(col)].width = width
