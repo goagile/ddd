6749b96ec0a007dbd06a8d5d18745724e0d63f86
khardi <kharchilindanila@gmail.com>
2017-11-20 09:13:05 +0300

    add one idea repository

--- source/hexagonal_architecture/_8_testing/tests.py
+++ source/hexagonal_architecture/_8_testing/tests.py
@@ -29,6 +29,17 @@ class TestCase(unittest.TestCase):
         with self.assertRaises(IdeaDoesNotExistException):
             use_case.execute(input_data)
 
+    def test_when_rating_an_idea_then_rating_should_be_added_and_idea_updated(self):
+        repository = OneIdeaRepository()
+        use_case = RateIdeaUseCase(repository)
+        input_data = RateIdeaRequest('1', 1)
+
+        result = use_case.execute(input_data)
+
+        actual = result.idea.rating
+        self.assertEqual(5, actual)
+        self.assertTrue(repository.update_called)
+
 
 class NotAvailableRepository(IdeaRepository):
 
@@ -46,3 +57,22 @@ class EmptyRepository(IdeaRepository):
 
     def update(self, idea: Idea):
         pass
+
+
+class OneIdeaRepository(IdeaRepository):
+
+    update_called = False
+
+    def find_by_id(self, idea_id: str):
+        idea = Idea()
+        idea.idea_id = idea_id
+        idea.title = 'Title'
+        idea.description = 'Description'
+        idea.rating = 4
+        idea.votes = 0
+        idea.email = 'xxx@xxx.com'
+
+        return idea
+
+    def update(self, idea: Idea):
+        self.update_called = True
