57bbb3cc7f8777066195acc3e79c865db7c8cfc4
khardi <kharchilindanila@gmail.com>
2017-11-23 22:10:59 +0300

    post status unpub

--- source/entities/posts/post.py
+++ source/entities/posts/post.py
@@ -1,8 +1,20 @@
 """
 
-    >>> content = 'xxx'
-    >>>
-    >>> p = Post()
+Нельзя создать пустой Post
+
+    >>> p = Post(None, None)
+    Traceback (most recent call last):
+    ...
+    AssertionError
+
+Заполняет заголовок и содержание
+
+    >>> content = 'Some info about writing the post'
+    >>> title = 'My new post'
+    >>> p = Post(content, title)
+
+    >>> str(p.status)
+    'UNPUBLISHED'
 
 """
 
@@ -13,6 +25,23 @@ class DateTimeImmutable:
         pass
 
 
+class Status:
+
+    def __init__(self, value):
+        self.__value = value
+
+    def __str__(self):
+        return self.__value
+
+    @classmethod
+    def published(cls):
+        return Status('PUBLISHED')
+
+    @classmethod
+    def unpublished(cls):
+        return Status('UNPUBLISHED')
+
+
 class Post:
 
     def __init__(self, content, title):
@@ -20,14 +49,36 @@ class Post:
         self.__title = None
         self.__set_content(content)
         self.__set_title(title)
+        self.__status = None
         self.unpublish()
-        self.create_at = DateTimeImmutable()
+        self.__create_at = DateTimeImmutable()
+        self.__publish_at = None
 
     def __set_content(self, content):
+        assert content
         self.__content = content
 
     def __set_title(self, title):
+        assert title
         self.__title = title
 
+    def __set_create_at(self, create_at: DateTimeImmutable):
+        assert self.__is_valid_date(create_at)
+        self.__create_at = create_at
+
+    def __set_publish_at(self, publish_at: DateTimeImmutable):
+        assert self.__is_valid_date(publish_at)
+        self.__publish_at = publish_at
+
+    def __is_valid_date(self, create_at: DateTimeImmutable):
+        return True
+
+    @property
+    def status(self):
+        return self.__status
+
+    def publish(self):
+        self.__status = Status.published()
+
     def unpublish(self):
-        pass
+        self.__status = Status.unpublished()
