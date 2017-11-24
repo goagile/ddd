b8a056f726eeede1025fffea3cfae42e25c74687
khardi <kharchilindanila@gmail.com>
2017-11-24 23:37:41 +0300

    need more spaces

--- source/tablo/__init__.py
+++ source/tablo/__init__.py
@@ -1,25 +1,6 @@
 
 
-class Tablo:
-
-    def __init__(self, headers):
-        self.headers = headers
-        self.rows = []
-
-    def __getattr__(self, item):
-        if item in self.headers:
-            return SplitTableColumn(item, self.rows)
-        return self.__getattribute__(item)
-
-    def __getitem__(self, item):
-        return self.rows[item]
-
-    def append(self, row_data):
-        row = SplitTableRow(self.headers, row_data)
-        self.rows.append(row)
-
-
-class SplitTableRow:
+class TabloRow:
 
     def __init__(self, headers, row_data):
         self.headers = headers
@@ -45,7 +26,7 @@ class InvalidColumnName(Exception):
     pass
 
 
-class SplitTableColumn:
+class TabloColumn:
 
     def __init__(self, name, rows):
         self.rows = self.collect_rows(name, rows)
@@ -64,3 +45,100 @@ class SplitTableColumn:
 
 class InvalidRowIndex(Exception):
     pass
+
+
+class Tablo:
+
+    Column = TabloColumn
+    Row = TabloRow
+
+    def __init__(self, headers):
+        self.headers = headers
+        self.rows = []
+
+    def __getattr__(self, item):
+        if item in self.headers:
+            return self.Column(item, self.rows)
+        return self.__getattribute__(item)
+
+    def __getitem__(self, item):
+        return self.rows[item]
+
+    def append(self, row_data):
+        row = self.Row(self.headers, row_data)
+        self.rows.append(row)
+
+
+class SplitTabloColumn(TabloColumn):
+
+    def __str__(self):
+        return joinsequence(self.rows)
+
+
+class SplitTabloRow(TabloRow):
+
+    def __init__(self, headers, row_data, margin=3):
+        super().__init__(headers, row_data)
+        self.margin = margin
+
+    def __str__(self):
+        formatted_sequence = []
+        for d in self.data:
+            x = self.__format(d)
+            formatted_sequence.append(x)
+        return joinrow(formatted_sequence)
+
+    def __format(self, data):
+        margin = max(self.margin, len(data))
+        format = Format(Align.Left, margin)
+        result = (data, format)
+        return result
+
+
+class SplitTablo(Tablo):
+
+    Column = SplitTabloColumn
+    Row = SplitTabloRow
+
+    def __init__(self, headers):
+        super().__init__(headers)
+        self.margin = 3
+
+    def append(self, row_data):
+        max_len = max(len(r) for r in row_data)
+        self.margin = max(self.margin, max_len)
+        row = self.Row(self.headers, row_data, margin=self.margin)
+        self.rows.append(row)
+
+
+class AlignTemplate:
+
+    def __init__(self, template):
+        self.template = template
+
+
+class Align:
+    Left = AlignTemplate('{}{{:{}}}')
+    Center = AlignTemplate('{}{{:^{}}}')
+    Right = AlignTemplate('{}{{:>{}}}')
+
+
+class Format:
+    def __init__(self, align, margin):
+        self.align = align
+        self.margin = margin
+
+
+# deprecated
+def joinsequence(sequence, separator='|', margin=3, align: AlignTemplate=Align.Center):
+    template = align.template.format(separator, margin) * len(sequence) + separator
+    result = template.format(*sequence)
+    return result
+
+
+def joinrow(formatted_sequence, separator='|'):
+    result = ''
+    for s, format in formatted_sequence:
+        template = format.align.template.format(separator, format.margin)
+        result += template.format(s)
+    return result + separator
---
+++ source/tablo/test_join_sequence.py
@@ -0,0 +1,16 @@
+"""
+
+    >>> from tablo import joinrow, Align, Format
+
+    >>> sequence = ['X', 'Y', 'Z', 'A', 'B']
+
+    >>> row = [
+    ...     ('4',   Format(Align.Left, margin=3)),
+    ...     ('Нет', Format(Align.Left, margin=3)),
+    ...     ('112.003', Format(Align.Left, margin=10)),
+    ... ]
+
+    >>> joinrow(row)
+    '|4  |Нет|112.003   |'
+
+"""
