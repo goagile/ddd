995c665bb6a4aaaae06838786723467e993e85aa
khardi <kharchilindanila@gmail.com>
2017-11-25 12:48:40 +0300

    klass builder save obj to file

--- source/klass/test_from_dict.py
+++ source/klass/test_from_dict.py
@@ -3,9 +3,12 @@
     >>> user_dict = {
     ...     'name': 'Вася',
     ...     'address': {
-    ...         'city': 'Moscow'
+    ...         'city': 'Moscow',
+    ...         'country': {
+    ...             'caption': 'Russia'
+    ...         }
     ...     },
-    ...     'заказы': [
+    ...     'orders': [
     ...         {
     ...             'part': 'Giutar',
     ...             'price': 100
@@ -13,7 +16,7 @@
     ...     ]
     ... }
 
-    >>> x = Klass.from_dict(user_dict)
+    >>> x = object_from_dict(user_dict)
     >>> x.name
     'Вася'
 
@@ -25,81 +28,95 @@
     ...     order.part, order.price
     ('Giutar', 100)
 
-    >>> x.build_klass_keyword()
+Строим описание класса
+
+    >>> builder = KlassBuilder()
+    >>> builder.build_klass_keyword(x)
     'class Klass:\\n'
 
-    >>> x.build_constructor()
-    '\\tdef __init__(self, address, name, orders):\\n'
+    >>> builder.build_constructor(x)
+    '    def __init__(self, address, name, orders):\\n'
 
-    >>> z = (
-    ...     '\\t\\t'
-    ...     'self.address = address'
-    ...     '\\n\\t\\t'
-    ...     'self.name = name'
-    ...     '\\n\\t\\t'
-    ...     'self.orders = orders'
-    ...     '\\n'
-    ... )
-    >>> z == x.build_fields()
-    True
+    >>> builder.build_fields(x)
+    '        self.address = address\\n        self.name = name\\n        self.orders = orders\\n'
 
-    >>> x.save('x.py')
+    >>> builder.save(x, 'x.py')
 
 """
 
 
 class Klass:
+    pass
+
+
+def object_from_dict(user_dict):
+    result = Klass()
+    for key, value in user_dict.items():
+        field = value
+        if isinstance(value, dict):
+            field = object_from_dict(value)
+        elif isinstance(value, list):
+            field = [object_from_dict(f) for f in value]
+        setattr(result, key, field)
+    return result
+
+
+class KlassBuilder:
 
     blank = '\n'
     tab = '    '
+    klass_keyword_template = 'class Klass:{blank}'
+    constructor_template = '{tabs}def __init__(self, {args}):{blank}'
+    field_template = '{tabs}self.{name} = {value}{blank}'
 
-    @classmethod
-    def from_dict(cls, user_dict):
-        result = Klass()
-        for key, value in user_dict.items():
-            field = value
-            if isinstance(value, dict):
-                field = cls.from_dict(value)
-            elif isinstance(value, list):
-                field = [cls.from_dict(f) for f in value]
-            setattr(result, key, field)
-        return result
-
-    def save(self, filename):
-        rows = [
-            self.build_klass_keyword(),
-            self.build_constructor(),
-            self.build_fields()
+    def build(self, obj):
+        result = [
+            self.blank,
+            self.blank,
+            self.build_klass_keyword(obj),
+            self.build_constructor(obj),
+            self.build_fields(obj)
         ]
-        with open(filename, 'w') as f:
-            f.writelines(rows)
-
-    def build_klass_keyword(self):
-        klass_keyword_template = 'class Klass:{blank}'
-        result = klass_keyword_template.format(blank=self.blank)
         return result
 
-    def build_constructor(self):
-        constructor_template = '{tabs}def __init__(self, {args}):{blank}'
-        result = constructor_template.format(
+    def build_klass_keyword(self, obj):
+        result = self.klass_keyword_template.format(blank=self.blank)
+        return result
+
+    def build_constructor(self, obj):
+        result = self.constructor_template.format(
             tabs=self.tab,
-            args=self.__get_args_names(),
+            args=self.__get_args_names(obj),
             blank=self.blank)
         return result
 
-    def __get_args_names(self):
-        return ', '.join(sorted(self.__dict__.keys()))
+    def __get_args_names(self, obj):
+        return ', '.join(sorted(obj.__dict__.keys()))
 
-    def build_fields(self):
-        result = [self.__build_field(name) for name in sorted(self.__dict__.keys())]
+    def extend_from_fields(self, obj, rows):
+        for field in obj.__dict__.values():
+            if isinstance(field, Klass):
+                rows.extend(self.build(field))
+                self.extend_from_fields(field, rows)
+            elif isinstance(field, list):
+                for f in field:
+                    self.extend_from_fields(f, rows)
+
+    def build_fields(self, obj):
+        result = [self.__build_field(name) for name in sorted(obj.__dict__.keys())]
         return ''.join(result)
 
     def __build_field(self, name):
-        field_template = '{tabs}self.{name} = {value}{blank}'
-        result = field_template.format(
+        result = self.field_template.format(
             tabs=self.tab + self.tab,
             name=name,
             value=name,
             blank=self.blank
         )
         return result
+
+    def save(self, obj, filename):
+        rows = self.build(obj)
+        self.extend_from_fields(obj, rows)
+        with open(filename, 'w') as f:
+            f.writelines(rows)
