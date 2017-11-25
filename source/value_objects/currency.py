7833ca4e5766602bb81c369bec8db2a6afc85f81
khardi <kharchilindanila@gmail.com>
2017-11-25 22:45:26 +0300

    money and currencies

---
+++ source/value_objects/currency.py
@@ -0,0 +1,41 @@
+"""
+
+    >>> usd = Currency('AAA22323')
+    Traceback (most recent call last):
+      ...
+    ValueError: Invalid Iso code
+
+    >>> usd = Currency('USD')
+    >>> usd
+    USD
+
+"""
+
+import re
+
+INVALID_ISO_CODE = 'Invalid Iso code'
+
+
+class Currency:
+
+    iso_code_pattern = re.compile('^[A-Z]{3}$')
+
+    def __init__(self, iso_code):
+        self.__iso_code = None
+        self.iso_code = iso_code
+
+    def __repr__(self):
+        return self.iso_code
+
+    @property
+    def iso_code(self):
+        return self.__iso_code
+
+    @iso_code.setter
+    def iso_code(self, value):
+        self.validate_iso_code(value)
+        self.__iso_code = value
+
+    def validate_iso_code(self, value):
+        if not self.iso_code_pattern.match(value):
+            raise ValueError(INVALID_ISO_CODE)
---
+++ source/value_objects/test.py
@@ -0,0 +1,16 @@
+"""
+
+measurements
+number
+date
+time
+money
+range
+text
+person full name
+color
+phone number
+address
+
+
+"""
