9776cb65eb960b2cd351232571984dfcb795c1a8
khardi <kharchilindanila@gmail.com>
2017-11-26 09:50:19 +0300

    test immutability of money

--- source/value_objects/money/tests/test_money.py
+++ source/value_objects/money/tests/test_money.py
@@ -5,40 +5,44 @@
     >>> from value_objects.money.money import Money
     >>> from value_objects.money.currency import USD, RUR
 
-Создаем объект деньги
+Создаем объект деньги.
+Проверяем его свойства, Печатем на консоли.
 
     >>> five_dollars = Money(5, USD)
     >>> five_dollars.amount
     5
     >>> five_dollars.currency
     USD
+    >>> five_dollars
+    $5
 
-Создаем копию объекта
+Создаем новые деньги на основе существующих.
+Деньги должны быть равны.
 
     >>> d5 = Money(5, USD)
     >>> m = Money.from_money(d5)
-    >>> d5 == m
+    >>> d5 == m; print(m, d5, sep=' == ')
     True
-    >>> print(m, d5, sep='; ')
-    $5; $5
+    $5 == $5
 
 Создаем ноль денег указанной валюты
 
-    >>> zero_dollars = Money.from_currency(USD)
-    >>> print(zero_dollars)
+    >>> zero = Money.from_currency(USD)
+    >>> print(zero)
     $0
 
-Добавляем деньги
+Увеличиваем сумму денег
 
-    >>> hundred_dollars = zero_dollars.increase_amount(100)
-    >>> hundred_dollars.amount
+    >>> zero = Money.from_currency(USD)
+    >>> hundred = zero.increase_amount(100)
+    >>> hundred.amount
     100
-    >>> print(hundred_dollars)
+    >>> print(hundred)
     $100
-    >>> print(hundred_dollars.increase_amount(20))
+    >>> print(hundred.increase_amount(20))
     $120
 
-Складываем деньги в разных валютах
+Складываем деньги
 
     >>> d100 = Money(100, USD)
     >>> d100
@@ -46,15 +50,27 @@
     >>> r2000 = Money(2000, RUR)
     >>> r2000
     ₽2000
+
+Сложить деньги в разной валюте не удастся
+
     >>> x = d100 + r2000
     Traceback (most recent call last):
       ...
     ValueError: USD != RUR Currency are not equal
+
+А вот в одинаковой можно
+
     >>> d20 = Money(20, USD)
     >>> d20
     $20
     >>> y = d100 + d20
     >>> y
     $120
+
+При сложении денег были созданы новые деньги.
+Существующие деньги не изменились.
+
+    >>> print(d20, y, sep=' != ')
+    $20 != $120
 
 """
