52833613eb14472a36344a992288ba6f9e64299b
khardi <kharchilindanila@gmail.com>
2017-11-29 20:41:23 +0300

    emulate rest post

--- source/tactical_patterns/services/sign_up.py
+++ source/tactical_patterns/services/sign_up.py
@@ -1,21 +1,84 @@
 """
 
-    >>> repository = Repository()
+SingUpService
+=============
+Служба уровня приложения
 
-    >>> request = dict(email='ivanov@gmail.com', password=123)
-    >>> controller = SignupController(repository)
-    >>> controller.sign_up_action(request)
+Создаем объекты на уровне приложения
+
+    >>> repository = Repository()
+    >>> handler = SignupHandler(repository)
+
+Эмулируем post запрос от клиента к уровню приложения
+
+    curl post http://localhost/sign_up_action/
+    >>> responce = handler.post(dict(email='ivanov@gmail.com', password=123))
+    >>> isinstance(responce, dict)
+    True
+    >>> responce['user_id']
+    11
+    >>> responce['email']
+    'ivanov@gmail.com'
+    >>> responce['password']
+    123
 
 
 """
 
 
 class Repository:
-    pass
+
+    ids = iter([11, 22, 33, 44, 55])
+
+    def user_by_email(self, email):
+        pass
+
+    def next_id(self):
+        return next(self.ids)
+
+    def add_user(self, user):
+        pass
 
 
 class SignUpUserRequest:
     def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
+class SignUpUserResponce:
+
+    def __init__(self, user):
+        self.user = user
+
+    @property
+    def user_id(self):
+        return self.user.user_id
+
+    @property
+    def email(self):
+        return self.user.email
+
+    @property
+    def password(self):
+        return self.user.password
+
+    def dumps(self):
+        result = {
+            'user_id': self.user_id,
+            'email': self.email,
+            'password': self.password,
+        }
+        return result
+
+
+class UserAlreadyExistException(Exception):
+    pass
+
+
+class User:
+    def __init__(self, user_id, email, password):
+        self.user_id = user_id
         self.email = email
         self.password = password
 
@@ -26,16 +89,42 @@ class SingUpService:
         self.repository = repository
 
     def execute(self, request: SignUpUserRequest):
-        return 2
+        user = self.repository.user_by_email(request.email)
+        if user:
+            raise UserAlreadyExistException
+
+        user = User(
+            self.repository.next_id(),
+            request.email,
+            request.password
+        )
+
+        self.repository.add_user(user)
+
+        request = SignUpUserResponce(user)
+
+        return request
 
 
-class SignupController:
+class SignupHandler:
 
     def __init__(self, repository):
         self.repository = repository
 
-    def sign_up_action(self, request: dict):
+    def post(self, request: dict):
+        response = None
+
+        email = request.get('email')
+        password = request.get('password')
+
         sign_up_service = SingUpService(self.repository)
-        response = sign_up_service.execute(SignUpUserRequest(
-            email=request.get('email'),
-            password=request.get('password')))
+
+        try:
+            response = sign_up_service.execute(SignUpUserRequest(email, password))
+        except UserAlreadyExistException:
+            print('User already exist')
+
+        if response:
+            response = response.dumps()
+
+        return response
