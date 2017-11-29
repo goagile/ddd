a072ade9caa3a7bbba4ed884463fa100225fe4c0
khardi <kharchilindanila@gmail.com>
2017-11-29 20:58:11 +0300

    add domain service

---
+++ source/tactical_patterns/services/domain_service.py
@@ -0,0 +1,40 @@
+"""
+
+User
+====
+Служба предметной области
+
+имеются объекты предметной области
+
+    >>> class User:
+    ...     def sign_up(self, username, password):
+    ...         pass
+
+    >>> class Cart:
+    ...     def create_order(self):
+    ...         pass
+
+Объект User не должен иметь обязанности по входу в систему
+Выделим для этого отдельную службу
+
+    >>> class SignUp:
+    ...     def execute(self, username, password):
+    ...         self.username = username
+    ...         self.password = password
+
+    >>> s = SignUp()
+    >>> s.execute('петрович', 123)
+
+"""
+
+
+class User:
+
+    def sign_up(self, username, password):
+        pass
+
+
+class Cart:
+
+    def create_order(self):
+        pass
