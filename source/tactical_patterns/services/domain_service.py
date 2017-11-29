7b1e620efbafc5555b3c6ffc56ed3f625f72d6bc
khardi <kharchilindanila@gmail.com>
2017-11-29 21:02:27 +0300

    cart service

--- source/tactical_patterns/services/domain_service.py
+++ source/tactical_patterns/services/domain_service.py
@@ -25,16 +25,18 @@ User
     >>> s = SignUp()
     >>> s.execute('петрович', 123)
 
+Служба, которая создает заказ на основании корзины
+
+    >>> class Cart:
+    ...     def __init__(self):
+    ...         pass
+
+    >>> class CreateOrderFromCart:
+    ...     def execute(self, cart):
+    ...         pass
+
+    >>> c = CreateOrderFromCart()
+    >>> cart = Cart()
+    >>> c.execute(cart)
+
 """
-
-
-class User:
-
-    def sign_up(self, username, password):
-        pass
-
-
-class Cart:
-
-    def create_order(self):
-        pass
