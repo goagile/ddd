43aa6512d9c3bc9773e28a00a42e2996d121e440
khardi <kharchilindanila@gmail.com>
2017-12-01 21:33:45 +0300

    rename nex id

--- source/calculator/step_2_sum_calculation_persist/calculation_repository.py
+++ source/calculator/step_2_sum_calculation_persist/calculation_repository.py
@@ -11,15 +11,16 @@ class CalculationsRepository:
         self.calculations = {}
         self.ids = iter([1, 2, 3])
 
+    def _next_id(self):
+        return next(self.ids)
+
     def append_calculation(self, calculation: Calculation):
-        calculation_id = self.next_id()
+        calculation_id = self._next_id()
         self.calculations[calculation_id] = calculation
         return calculation_id
 
-    def next_id(self):
-        return next(self.ids)
-
-    def find_calculation_by_id(self, calculation_id):
+    def find_calculation_by_id(self, calculation_id) -> Calculation:
         calculation = self.calculations.get(calculation_id)
         if not calculation:
             raise NotFoundCalculation
+        return calculation
