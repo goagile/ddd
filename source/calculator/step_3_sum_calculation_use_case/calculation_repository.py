871676fd943207bf09e4d9874c25bf923d3596c0
khardi <kharchilindanila@gmail.com>
2017-12-02 12:41:26 +0300

    fix calculation dumps loads

--- source/calculator/step_3_sum_calculation_use_case/calculation_repository.py
+++ source/calculator/step_3_sum_calculation_use_case/calculation_repository.py
@@ -16,11 +16,12 @@ class CalculationRepository:
 
     def append_calculation(self, calculation: Calculation):
         calculation_id = self._next_id()
-        self.calculations[calculation_id] = calculation
+        self.calculations[calculation_id] = calculation.dumps()
         return calculation_id
 
     def find_calculation_by_id(self, calculation_id) -> Calculation:
         calculation = self.calculations.get(calculation_id)
         if not calculation:
             raise NotFoundCalculation
-        return calculation
+        result = Calculation.load(calculation)
+        return result
